db619b450dc7fde8973fcc44f0480b0872965c50
Jokubas <jokubas@shape.dk>
2021-02-25 13:52:30 +0100

    created a singleton dda engine and some initial functions

---
+++ Assets/Scripts/DDAA/DDAEngine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class DDAEngine
+{
+
+    // --------------------------------- //
+    // Singleton related implementation //
+    //----------------------------------//
+
+    private static DDAEngine instance = null;
+    private static readonly object padlock = new object();
+
+    private DDAEngine()
+    {
+
+    }
+
+    public static DDAEngine Instance { 
+        get 
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new DDAEngine();
+                }
+                return instance;
+            }
+        } 
+    }
+
+    // Used functions 
+    // Call them using DDAEngine.Instance.FunctionName
+
+    private float difficultiesPointGlobal = 10;
+
+    public void AdjustDPG(float additiveValue)
+    {
+        difficultiesPointGlobal += additiveValue;
+    }
+
+    public float CalculateInGameValue(float point, float dpgContribution, float minValue = 0f, float inGameValue = 0f) 
+    {
+        return minValue + point + difficultiesPointGlobal * dpgContribution + inGameValue;
+    }
+}
