0f0783deec94afecbe121f87e6c56cabdb75a54b
Jokubas <jokubas@shape.dk>
2021-02-28 16:53:40 +0100

    Set up test scripts

---
+++ Assets/Scripts/DDAA/TestScripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int killCount;
+
+    private static readonly int initialTime = 5;
+    private int time = initialTime;
+
+    [SerializeField] private TextMesh timerText;
+    [SerializeField] private TextMesh killCountText;
+
+    private void Start()
+    {
+        InvokeRepeating("ControlTimer", 1, 0);
+    }
+
+    private void ControlTimer()
+    {
+        time--;
+        timerText.text = time.ToString();
+        if (time == 0)
+        {
+            AdjustDifficulty();
+            time = initialTime;
+            timerText.text = time.ToString();
+        }
+    }
+
+    public void OnInreaseClicked()
+    {
+        killCount++;
+        killCountText.text = killCount.ToString();
+    }
+
+    public void OnDicreaseClicked()
+    {
+        if (killCount < 0)
+        {
+            killCount--;
+            killCountText.text = killCount.ToString();
+        }
+    }
+
+    private void AdjustDifficulty()
+    {
+        KillCountCondition.Instance.ConditionValue = killCount;
+        ReloadDDAA.Instance.AdjustReloadTime();
+    }
+}
---
+++ Assets/Scripts/DDAA/TestScripts/ReloadObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadObject : MonoBehaviour, ReloadDDAA.IReloadChangeListener
+{
+
+    private float reloadTime = ReloadDDAA.Instance.reloadTime;
+
+    [SerializeField] private TextMesh reloadTimeText;
+
+    private void Awake()
+    {
+        ReloadDDAA.Instance.SetReloadListener(this);
+    }
+
+    private void Start()
+    {
+        reloadTimeText.text = reloadTime.ToString();
+    }
+
+    public void OnReloadTimeChanged()
+    {
+        Debug.Log("Reload time changed. New reload time is " + reloadTime.ToString());
+        reloadTimeText.text = reloadTime.ToString();
+    }   
+}
