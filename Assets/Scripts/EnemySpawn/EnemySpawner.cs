c11a02ff07003978689a0a354bf1bf57a71f73d4
Jokubas <jokubas@shape.dk>
2021-03-27 13:26:39 +0100

    applied changes to EnemySpawnPoint

--- Assets/Scripts/EnemySpawn/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawn/EnemySpawner.cs
@@ -132,7 +132,6 @@ public class EnemySpawner : MonoBehaviourPunCallbacks, IValueChangeListener
                 enemiesLeftToSpawnForArea--;
                 yield return new WaitForSeconds(spawnIntervalForArea);
             }
-            
         }
         else
         {
