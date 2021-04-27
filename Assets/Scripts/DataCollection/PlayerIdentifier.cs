007746df6acca2805e18f48d7ec24f525f0ac62f
latedude2 <simasc10@gmail.com>
2021-04-27 15:19:36 +0200

    Share player identifier with other players

--- Assets/Scripts/DataCollection/PlayerIdentifier.cs
+++ Assets/Scripts/DataCollection/PlayerIdentifier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using UnityEngine.UI;
+using Photon.Pun.Demo.PunBasics;
 
 
 public class PlayerIdentifier : MonoBehaviour
@@ -10,10 +11,24 @@ public class PlayerIdentifier : MonoBehaviour
     [DllImport("__Internal")]
     public static extern void RequestPlayerIdentifier();
 
-    public string playerIdentifier;
+    public string playerIdentifier = "";
 
     void GetPlayerIdentifier(string data)
     {
         playerIdentifier = data;
+    }
+
+    public void SetPlayerIdentifiers()
+    {
+        List<GameObject> players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+        foreach (GameObject player in players)
+        {
+            if (player.GetComponent<PlayerManager>().IsPlayerLocal())
+            {
+                player.GetComponent<PlayerManager>().CallGetPlayerIdentifier(playerIdentifier);            //we send other players our player identifier
+                Debug.Log("Identified player: " + playerIdentifier);
+            }
+
+        }
     }
 }
