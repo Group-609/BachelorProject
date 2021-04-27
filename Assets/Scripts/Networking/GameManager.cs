cbdd97cfcf1cf69b168ebc2125020c64d90facf5
Jokubas <jokubas@shape.dk>
2021-04-27 18:08:10 +0200

    finding required gameobjects for all players

--- Assets/Scripts/Networking/GameManager.cs
+++ Assets/Scripts/Networking/GameManager.cs
@@ -114,7 +114,6 @@ namespace Photon.Pun.Demo.PunBasics
 				{
 					Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
 					StartCoroutine(LevelStartReset(initialSpawnPosition));
-					PlayerManager.LocalPlayerManager.FindNewGameObjects();
 				}
 			}
 			if (PhotonNetwork.IsMasterClient)
@@ -125,6 +124,8 @@ namespace Photon.Pun.Demo.PunBasics
 			Debug.Log("Is DDA enabled: " + IsDDAEnabled);
 
 			InvokeRepeating(nameof(RefreshPlayers), 0, 1f);
+			
+			players.ForEach(player => player.GetComponent<PlayerManager>().FindNewGameObjects());
 		}
 
 		/// <summary>
