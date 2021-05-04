3c00aead7d5a440d9511058ebf72d3832a9236af
Mikkel Munk <cloaked@live.dk>
2021-05-04 16:53:30 +0200

    Added more Howard

---
+++ Assets/Scripts/rotataVida.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotataVida : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0, -(30 * Time.deltaTime), 0, Space.Self);
+    }
+}
