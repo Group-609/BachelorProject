520ab47bc424e97e1624f0671330a06e4684eddd
Jokubas <jokubas@shape.dk>
2021-05-04 10:20:31 +0200

    adjusted DDAAs

--- Assets/Scripts/DDAA/Variables/PlayerPainballDamageDDAA.cs
+++ Assets/Scripts/DDAA/Variables/PlayerPainballDamageDDAA.cs
@@ -41,14 +41,9 @@ public sealed class PlayerPainballDamageDDAA : IDDAA
     private static readonly float paintballDamagePointContribution = 1f;
 
     // IMPORTANT! Both arrays have to be the same length
-    private static readonly float[] stunCountDiffPointAdditiveValues = new float[] { -2f, -1f, 0f, 1f, 2f }; // additive values to point directly
-    private static readonly float[] stunCountDiffMultiplierAdditiveValues = new float[] { -0.3f, -0.15f, 0f, 0.15f, 0.3f }; // additive values to multiplier
-
-    private static readonly float[] damageReceivedDiffPointAdditiveValues = new float[] { -2f, -1f, 0f, 1f, 2f }; // additive values to point directly
-    private static readonly float[] damageReceivedDiffMultiplierAdditiveValues = new float[] { -0.3f, -0.15f, 0f, 0.15f, 0.3f }; // additive values to multiplier
-
-    private static readonly float[] defeatedEnemiesDiffPointAdditiveValues = new float[] { 2f, 1f, 0f, -1f, -2f }; // additive values to point directly
-    private static readonly float[] defeatedEnemiesDiffMultiplierAdditiveValues = new float[] { 0.2f, 0.11f, 0f, -0.1f, -0.2f }; // additive values to multiplier
+    private static readonly float[] stunCountDiffMultiplierAdditiveValues = new float[] { -0.1f, -0.05f, 0f, 0.05f, 0.1f }; // additive values to multiplier
+    private static readonly float[] damageReceivedDiffMultiplierAdditiveValues = new float[] { -0.1f, -0.05f, 0f, 0.05f, 0.1f }; // additive values to multiplier
+    private static readonly float[] defeatedEnemiesDiffMultiplierAdditiveValues = new float[] { 0.2f, 0.1f, 0f, -0.1f, -0.2f }; // additive values to multiplier
 
     // Mutable parameters. 
     // Do not ajust these, they will change during the gameplay
