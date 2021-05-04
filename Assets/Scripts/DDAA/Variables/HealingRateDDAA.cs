24eb463226024a0c0a4ce42c78ec3c96629bef07
Jokubas <jokubas@shape.dk>
2021-05-04 15:45:57 +0200

    increase the healing rate a little

--- Assets/Scripts/DDAA/Variables/HealingRateDDAA.cs
+++ Assets/Scripts/DDAA/Variables/HealingRateDDAA.cs
@@ -35,7 +35,7 @@ public sealed class HealingRateDDAA : IDDAA
     // All of the parameters below are the ones to change, when adjusting the DDA (unless there's a bug)
 
     //static parameters
-    private static readonly float baseHealingPoint = 4f;
+    private static readonly float baseHealingPoint = 5f;
     public static readonly float minHealingRate = 4f;
     public static readonly float maxHealingRate = 20f;
     // DPG is individual, so it shouldn't change team based DDA variables (based on one player's individual skill, spawn amount shouldn't change
@@ -43,7 +43,6 @@ public sealed class HealingRateDDAA : IDDAA
     private static readonly float healingPointContribution = 1f;
 
     // IMPORTANT! Both arrays have to be the same length
-    private static readonly float[] levelProgressionPointAdditiveValues = new float[] { -2f, -1f, 0f, 1f, 2f }; // additive values to point directly
     private static readonly float[] levelProgressionMultiplierAdditiveValues = new float[] { -0.5f, -0.2f, 0f, 0.2f, 0.5f }; // additive values to multiplier
 
     // Mutable parameters. 
